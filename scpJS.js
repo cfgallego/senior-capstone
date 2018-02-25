36d8dd08d81a9f6f819bb9df19ed6ccfca7e3d53
LAPTOP-AEN7LA5S\Darian <dalynch@augusta.edu>
2018-02-25 14:16:18 -0500

    got the information to preload on the next page.

--- scpJS.js
+++ scpJS.js
@@ -14,3 +14,31 @@ window.onclick = function(e) {
       }
   }
 }
+
+// Sends emergency groups selected to next page.
+function selectedGroups() {
+	var checkedboxx ="";
+	var sgroups = document.getElementsByTagName('input');
+	for(var i =0; sgroups[i]; i++){
+		if(sgroups[i].checked){
+			checkedboxx += sgroups[i].value += ", ";
+				
+		}
+	}
+	checkedboxx = checkedboxx.slice(0,-2);
+	window.location.href = "next.html?" + checkedboxx;
+}
+
+//Preloads all the text boxes with the information
+function getGroups(){
+	var today = (new Date().toString().split(' ').slice(1,4));
+	var now = new Date().toLocaleString().slice(11); 
+	var queryString = decodeURIComponent(window.location.search);
+	queryString = queryString.substring(1);
+	var queries = queryString.split("&");
+	
+	document.getElementById("emergency").value = queries;
+	document.getElementById("date").value = today;
+	document.getElementById("time").value = now;
+}
+
