525e47efbd414420ee80ec3fe3a0796bf9a9e3bc
MIKIPC\Miki Racho <mracho@augusta.edu>
2018-03-29 00:39:16 -0400

    pushing latest working copy

---
+++ Capstone2018/ResponderManagement/ScriptsAppSpecific/controllers/initialController.js
@@ -0,0 +1,6 @@
+﻿angular.module("app").controller("initialController", ['$scope', 'AppServices', '$http', '$window', '$filter', '$rootScope', function ($scope, appServices, $http, $window, $filter, $rootScope) {
+    var self = this;
+    console.log("initial controller");
+
+    $rootScope.id = 0;
+}]);
