2cc7d3f6fabe2edc3392ff80522b4aa550c4dae4
MIKIPC\Miki Racho <mracho@augusta.edu>
2018-03-30 04:18:00 -0400

    added form validation on add and edit volunteer form

---
+++ Capstone2018/ResponderManagement/ScriptsAppSpecific/appServices.js
@@ -0,0 +1,46 @@
+﻿angular.module("app").factory("AppServices", [
+    '$http', function ($http) {
+        return {
+            // VolunteerController functions
+            addNewVolunteer: function (newVol) {
+                return $http.post("api/ResponderManagement/volunteers/addVolunteer", newVol);
+            },
+
+            getVolunteers: function () {
+                return $http.get("api/ResponderManagement/volunteers/getVolunteers");
+            },
+
+            getVolunteerByID: function (id) {
+                return $http.get("api/ResponderManagement/volunteers/getVolunteerByID?id=" + id);
+            },
+
+            //getVolunteerByGroup: function (group) {
+            //    return $http.get("api/ResponderManagement/volunteers/getVolunteer?group=" + group);
+            //},
+
+            //getVolunteerByName: function (name) {
+            //    return $http.get("api/ResponderManagement/volunteers/getVolunteer?name=" + name);
+            //},
+
+            // updateVolunteer
+            editVolunteer: function (vol) {
+                return $http.put("api/ResponderManagement/volunteers/editVolunteer", vol);
+            },
+
+            // deleteVolunteer
+            deleteVolunteer: function (id) {
+                return $http.delete("api/ResponderManagement/volunteers/deleteVolunteer?id=" + id);
+            },
+
+
+            // EmergencyController functions
+            getEmergencies: function () {
+                return $http.get("api/ResponderManagement/emergencies/getEmergencies");
+            },
+
+            // SkillController functions
+            getSkills: function () {
+                return $http.get("api/ResponderManagement/skills/getSkills");
+            }
+        }
+    }]);
