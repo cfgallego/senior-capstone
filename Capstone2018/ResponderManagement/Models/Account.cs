5607a98a9a06818cbfcc9aa8aed58a2783ccceaa
MIKIPC\Miki Racho <mracho@augusta.edu>
2018-04-20 17:32:05 -0400

    pushing project. (login, email, updated history table)

---
+++ Capstone2018/ResponderManagement/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResponderManagement.Models
+{
+    public class Account
+    {
+        [Key]
+        public int DirectoryID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Capstone2018/ResponderManagement/Models/Emergency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResponderManagement.Models
+{
+    public class Emergency
+    {
+        [Key]
+        public int EmergencyID { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Skill> Skills { get; set; }
+
+        public Emergency()
+        {
+            Skills = new List<Skill>();
+        }
+    }
+}
