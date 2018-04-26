a6edd4ec4c2a42e9c8d4e88594d46f70d6913607
MIKIPC\Miki Racho <mracho@augusta.edu>
2018-04-26 02:35:22 -0400

    added pagination on history page

--- Capstone2018/ResponderManagement/ScriptsAppSpecific/controllers/historyController.js
+++ Capstone2018/ResponderManagement/ScriptsAppSpecific/controllers/historyController.js
@@ -12,8 +12,29 @@
             self.history = response.data;
             console.log(self.history);
             self.loadDb = false;
+
+            console.log(self.history.length);
+
+
+            // pagination
+            self.currentPage = 0;
+            self.pageSize = 7;
+
+            self.numberOfPages = function () {
+                return Math.ceil(self.history.length / self.pageSize);
+            }
+            console.log(Math.ceil(self.history.length / self.pageSize));
         });
     };
 
     self.loadHistory();
 }]);
+
+
+// limitTo filter is built-in
+angular.module("app").filter('startFrom', function () {
+    return function (input, start) {
+        start = +start;
+        return input.slice(start);
+    }
+});
